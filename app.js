6fcb398658fd37c47b07b3ee19a8d989eb9d3f55
mspake <morgana.spake@gmail.com>
2019-03-07 11:26:07 -0800

    Updates JS with stretch goals, updated HTML top 10 list

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
 'use strict';
 
-//thoughtful, descriptive console logs
-//doneish//user story for the ehobbiesperience of interacting with the page
-//done//-->fun, intersting, informative ehobbiesperience learning about the page owner
-//done//add a question that takes numeric input
-//done//-->tell if too high, too low, limit to ehobbiesactly 4 guesses
-//add a question with multiple correct answers (stored in an array)
-//done//-->6 tries to get one correct, display all correct answers when one is guessed or guess limit is reached
-//tally total correct answers given by user and display their total (out of) at the end.
-//stretch:
-//put all questions, answers, responses for first 5 questions into arrays (or single multidimensional array)
-//make the number guessing use a random number
-
-
-
 //declare variable yes_answers
 //yes_answers is an array containing various forms of yes
 //declare variable no_answers
@@ -31,175 +17,105 @@ var name;
 name = prompt('Hello visitor, welcome to my site. What\'s your name?');
 
 
+//stretch goal version of the first five questions, using arrays
 
-//declare variable lives_in_seattle
-//lives_in_seattle is assigned the returned value of the prompt
-var lives_in_seattle;
-lives_in_seattle = prompt('Welcome ' + name + ', let\'s play a guessing game! I\'ll start easy. Do I live in Seattle?').toLowerCase();
+//print a welcome mesaage to the user
+alert('Welcome ' + name + ', let\'s play a guessing game!').toLowerCase();
 
-//print lives_in_seattle to the console
-console.log('Does the user think I live in seattle? ' + lives_in_seattle);
+//declare variable questions that is a multidimensional array
+//each element array holds: the text of the question, the correct answer, an empty string (to hold user'd guess), and a response
+var questions = [
+  ['Do I live in Seattle?', 'yes', '', 'I do live in Seattle.'],
+  ['Do I like the weather here?', 'yes', '', 'I love the grey and rainy weather.'],
+  ['Do I drink coffee?', 'no', '', 'I much prefer tea.'],
+  ['Do I like to read?', 'yes', '', 'I love reading pretty much anything.'],
+  ['Do I drive?', 'no', '', 'It\'s just me, my feet, and a bus pass.']
+];
 
-//check if the value stored in lives_in_seattle is present in the yes_answers array or the no_answers array
-//prints a different message to the user depending on the result of the check
-if (yes_answers.includes(lives_in_seattle)) {
-  alert('Very good!');
-  console.log('They are correct');
-  //increments total correct guesses
-  total_correct_guesses++;
-} else if (no_answers.includes(lives_in_seattle)) {
-  alert('I\'m very sorry, that\'s incorrect.');
-  console.log('They are incorrect');
-} else {
-  alert('I\'m not sure how to interpret your guess. But, yes, I live in Seattle.');
+
+//check if their guess is contained in yes_answers or no_answers
+//if yes, assign it the coresponding 'yes' or 'no' value
+//if not, assign it a value of 'unclear'
+for (var i = 0; i < questions.length; i++) {
+  //the user is asked the question and their answer is stored in the coesponding array
+  questions[i][2] = prompt(questions[i][0]).toLowerCase();
+  //print guess to console
+  console.log(questions[i][0] + ' The user guessed: ' + questions[i][2]);
+
+  //check if their answer a yes, a no, or something else
+  //assign it a coresponding value
+  if (yes_answers.includes(questions[i][2])) {
+    questions[i][2] = 'yes';
+  } else if (no_answers.includes(questions[i][2])) {
+    questions[i][2] = 'no';
+  } else {
+    questions[i][2] = 'unclear';
+  }
+
+  //check if their answer matches the correct answer
+  //print a different message to the user and the console based on correct, not correct, unclear guesses
+  if (questions[i][2] === questions[i][1]) {
+    alert('Correct! ' + questions[i][3]);
+    console.log('They guessed correctly.');
+    //increment total_correct_guesses
+    total_correct_guesses++;
+  } else if (questions[i][2] === 'unclear') {
+    alert('I\'m not sure how to interpret your guess. But the answer is ' + questions[i][1] + '. ' + questions[i][3]);
+    console.log('Answer unclear');
+  } else {
+    alert('I\'m sorry, that\'s incorrect. ' + questions[i][3]);
+    console.log('They guessed incorrectly.');
+  }
+
+  //print total_correct_guesses to the console
+  console.log(total_correct_guesses);
 }
 
-//print total_correct_guesses to the console
-console.log('Total questions answered correctly: ' + total_correct_guesses);
 
-
-
-//declare variable likes_the_weather
-//likes_the_weather is assigned the returned value of the prompt
-var likes_the_weather;
-likes_the_weather = prompt('Do I like the weather here in Seattle?').toLowerCase();
-
-//print likes_the_weather to the console
-console.log('Does the user think I like the weather in Seattle? ' + likes_the_weather);
-
-//check if the value stored in likes_the_weather is present in the yes_answers array or the no_answers array
-//prints a different message to the user depending on the result of the check
-if (yes_answers.includes(likes_the_weather)) {
-  alert('Yes! It\'s so beautiful and rainy!');
-  console.log('They are correct');
-  //increments total correct guesses
-  total_correct_guesses++;
-} else if (no_answers.includes(likes_the_weather)) {
-  alert('Actually, I do. I love the grey, rainy weather.');
-  console.log('They are incorrect');
-} else {
-  alert('I\'m not sure how to interpret your guess. But, yes, I love the weather here.');
-}
-
-//print total_correct_guesses to the console
-console.log('Total questions answered correctly: ' + total_correct_guesses);
-
-//declare variable drinks_coffee
-//drinks_coffee is assigned the value returned by the prompt
-var drinks_coffee;
-drinks_coffee = prompt('Do I drink coffee?').toLowerCase();
-
-//prints drinks_coffee to the console
-console.log('Does the user think I drink coffee? ' + drinks_coffee);
-
-//check if the value stored in drinks_coffee is present in the yes_answers array or the no_answers array
-//prints a different message to the user depending on the result of the check
-if (yes_answers.includes(drinks_coffee)) {
-  alert('Actually, no. Although I live in Seattle, I\'m not a coffee person. I much prefer tea.');
-  console.log('They are incorrect');
-} else if (no_answers.includes(drinks_coffee)) {
-  alert('Nice! You\'re right, I much prefer tea.');
-  console.log('They are correct');
-  //increments total correct guesses
-  total_correct_guesses++;
-} else {
-  alert('I\'m not sure how to interpret your guess. But the answer is no, I much perfer tea to coffee.');
-}
-
-//print total_correct_guesses to the console
-console.log('Total questions answered correctly: ' + total_correct_guesses);
-
-//declare variable likes_to_read
-//likes_to_read is assigned the returned value of the prompt
-var likes_to_read;
-likes_to_read = prompt('Do I like to read? ').toLowerCase();
-
-//prints likes_to_read to the console
-console.log('Does the user think I like to read? ' + likes_to_read);
-
-//check if the value stored in likes_to_read is present in the yes_answers array or the no_answers array
-//prints a different message to the user depending on the result of the check
-if (yes_answers.includes(likes_to_read)) {
-  alert('Heck yeah I do!');
-  console.log('They are correct');
-  //increments total correct guesses
-  total_correct_guesses++;
-} else if (no_answers.includes(likes_to_read)) {
-  alert('Incorrect. I love reading.');
-  console.log('They are incorrect');
-} else {
-  alert('I\'m not sure how to interpret your guess. But I love reading.');
-}
-
-//print total_correct_guesses to the console
-console.log('Total questions answered correctly: ' + total_correct_guesses);
-
-//declare variable drives_a_car
-//drives_a_car gets assigned the returned value of the prompt
-var drives_a_car;
-drives_a_car = prompt('Do I drive a car?').toLowerCase();
-
-//print drives_a_car to the console
-console.log('Does the user think I drive? ' + drives_a_car);
-
-//check if the value stored in drives_a_car is present in the yes_answers array or the no_answers array
-//prints a different message to the user depending on the result of the check
-if (yes_answers.includes(drives_a_car)) {
-  alert('Nope! It\'s just me, my feet, and a bus pass.');
-  console.log('They are incorrect');
-} else if (no_answers.includes(drives_a_car)) {
-  alert('Correct! I walk and bus everywhere I can (though sometimes I\'ll catch a ride with a friend.)');
-  console.log('They are correct');
-  //increments total correct guesses
-  total_correct_guesses++;
-} else {
-  alert('I\'m not sure how to interpret your guess. But the answer is no, it\'s just me, my feet, and a bus pass. (though sometimes I\'ll catch a ride with a friend.)');
-}
-
-//print total_correct_guesses to the console
-console.log('Total questions answered correctly: ' + total_correct_guesses);
-
-
-
-//What follows is a question that takes numeric input
+//add a sixth question that takes numeric input
 //messages tell the user if their guess is too high or too low
 //limit exactly 4 guesses
 
+//random number version of question 6
+
 //variable declaration:
-//declare variable years_in_seattle and assign it the value of 6
-//declare variable years_guessed
+//declare variable random_number and assign it a random whole number value between 1 and 10
+//declare variable number_guessed
 //declare variable guess and assign it a value of 0
 //declare variable got_it and assign it a value of false
-//declare variable guesses_left and assign it a message fragment
-var years_in_seattle = 6;
-var years_guessed;
+//declare variable guesses_left and assign it a string value (message fragment)
+var random_number = Math.floor((Math.random() * 10) + 1); //syntax found in "JavaScript and JQuery" by Jon Ducket
+var number_guessed;
 var guess = 0;
 var got_it = false;
 var guesses_left = 'I\'ll give you 4 guesses';
 
+//print random number to console
+console.log(random_number);
+
 //do..while loop
-//value stored in years_guessed (as returned by prompt) is compared to the value stored in years_in_seattle
+//value stored in number_guessed (as returned by prompt) is compared to the value stored in random_number
 //looking for too high, too low, or correct
 //limit of 4 guesses
 //prints a different message to the user if guess too high, too low, or correct
 do {
-  years_guessed = prompt('How many years have I lived in Seattle? ' + guesses_left + '.');
+  number_guessed = prompt('I\'m thinking of a number between 1 and 10. Can you guess which one? ' + guesses_left + '.');
 
-  //prints years_guessed to the console
-  console.log('User guessed I\'ve been in Seattle for ' + years_guessed + ' years.');
+  //prints number_guessed to the console
+  console.log('User guessed: ' + number_guessed);
 
-  //compares value stored in guess to value stored in years_in_seattle
+  //compares value stored in guess to value stored in random_number
   //tells the user if their guess is too high or too low
   //prints a message to the user depending on what they guessed
   //prints too high, too low, or correct to the console
-  if (years_guessed > years_in_seattle) {
+  if (number_guessed > random_number) {
     alert('Too high!');
     console.log('Guess was too high');
-  } else if (years_guessed < years_in_seattle) {
+  } else if (number_guessed < random_number) {
     alert('Too low!');
     console.log('Guess was too low');
-  } else if (years_guessed == years_in_seattle) {
-    alert('Correct! I\'ve been here 6 years.');
+  } else if (number_guessed == random_number) {
+    alert('Correct! I was thinking ' + random_number);
     console.log('They are correct');
     //assigns a value of true to variable got_it if the user guesses correctly
     got_it = true;
@@ -211,42 +127,52 @@ do {
 
   //sets the limit to the number of guesses to 4
   if (guess === 3 && got_it === false) {
-    alert('I\'m sorry, you\'ve run out of guesses. The answer is 6 years.');
+    alert('I\'m sorry, you\'ve run out of guesses. I was thinking of ' + random_number);
   }
   //increments guess
-  //print remaining number of to user and to console
+  //print remaining guesses message to user and to console
   guess++;
   guesses_left = ('You have ' + (4 - guess) + ' guesses left');
   console.log('Guesses left: ' + (4 - guess));
 
 } while (guess < 4 && got_it === false);
 
-//print total_correct_guesses to the console
-console.log('Total questions answered correctly: ' + total_correct_guesses);
 
 
 
+//add a seventh question with multiple correct answers (stored in an array)
+//limit 6 tries to get one correct
+//display all correct answers when one is guessed or guess limit is reached
 
-//add a question with multiple correct answers (stored in an array)
-//--> 6 tries to get one correct, display all correct answers when one is guessed or guess limit is reached
-
-//declare variable ___, ___ is an array containing all the things
-var hobbies = ['reading', 'sewing', 'jewelry making', 'embroidery', 'writting', 'painting', 'drawing'];
+//declare variable hobbies and assign it an array of strings (list of hobbies)
+//declare variable answer
+//declare variable found_one and assign it a value of false
+//declare variable attempts and assign it a value of 0
+//declare variable guesses_remaining and assign it a string value (message fragment)
+var hobbies = ['reading', 'sewing', 'jewelry making', 'embroidery', 'writting', 'painting', 'drawing', 'photography'];
 var answer;
 var found_one = false;
 var attempts = 0;
 var guesses_remaining = 'I\'ll give you 6 guesses';
 
 
-//ask for an answer until the user gets one right or runs out of guesses
+//do..while loop
+//answer is assigned the returned value of the prompt and compared to the elemnts in the hobbies array
 //limit 6 guesses
-//check if array hobbies contains the value contained in answer
+//print a different message to the user depending on if answer is included or not
 do {
   answer = prompt('Can you guess one of my hobbies? ' + guesses_remaining + '.').toLowerCase();
+
+  //prints answer to the console
   console.log(answer);
+
+  //check if the hobbies array includes an element with the same value as answer
+  //tells the user if they got it right or not
+  //prints correct or not correct to the console
   if (hobbies.includes(answer)) {
     alert('Well done!');
     console.log('A correct guess');
+    //found_one is assigned a value of true
     found_one = true;
     //increments total correct guesses
     total_correct_guesses++;
@@ -254,14 +180,21 @@ do {
     alert('Nope. (Well...it\'s not one I listed here anyway...)');
     console.log('Inncorrect (probably) guess');
   }
+
+  //sets the limit to the number of guesses to 6
   if (attempts === 5 && found_one === false) {
     alert('I\'m sorry, you\'ve run out of guesses.');
   }
+
+  //increments attempts
   attempts++;
+
+  //prints remaing guesses messsage to user and console
   guesses_remaining = ('You have ' + (6 - attempts) + ' guesses left');
+  console.log('Guesses left: ' + (6 - attempts));
+
 
 } while (attempts < 6 && found_one === false);
-
 
 //define variable hobbies_list and assign it a string value that is a list of all the elements in the hobbies array
 //syntax found on Stack Overflow (https://stackoverflow.com/questions/13939573/how-to-add-spaces-between-array-items-javascript)
@@ -272,33 +205,3 @@ alert('Here they are: ' + hobbies_list + '. There are more, but the list was get
 //print total correct guesses to the user and the console
 alert('Thanks for playing, ' + name + '! You got ' + total_correct_guesses + ' out of ' + number_of_questions + ' correct.');
 console.log('Total questions answered correctly: ' + total_correct_guesses);
-
-/*
-//section framework
-
-var ;
- = prompt().toLowerCase();
-console.log();
-
-if (yes_answers.includes()) {
-    alert();
-} else if (no_answers.includes()) {
-    alert();
-} else {
-    alert();
-}
-*/
-
-/*
-//In class demo code notes
-
-var guess = prompt();
-
-var correct_answer = 'no';
-
-if (guess === correct_answer) {
-    alert('Yeah!');
-} else {
-    alert('Nope, try again.');
-}
-*/
